3396581b11516ce4f4eb00fc9106b5df6f7e3481
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-02-10 23:30:51 +0430

    feat: set fake  static data

---
+++ src/data/dashboardData.js
@@ -0,0 +1,13 @@
+export const stats = [
+    {label: "Users", value: "1,221", icon: "people"},
+    {label: "Orders", value: "298", icon: "shopping"},
+    {label: "Revenue", value: "$12,350", icon: "money"},
+    {label: "Feedback", value: "78", icon: "chat"},
+]
+
+export const users = [
+    {id: 1, name: "Sadaf Amiri", email: "sadaf@example.com", role: "Admin", status: "Active"},
+    {id: 2, name: "Shabnam Ahmadi", email: "shabnam@exaple.com", role: "Editor", status: "Active"},
+     {id: 3, name: "Nargas hamidi", email: "nargas@example.com", role: "Viewer", status: "Pending"},
+    {id: 4, name: "Sonam Sadat", email: "sonam@exaple.com", role: "Viewer", status: "Blocked"},
+]
