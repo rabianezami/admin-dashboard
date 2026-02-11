195b3082a017cb609834ae2ec3063d6cde9e46d6
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-02-11 10:53:53 +0430

    Call Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "@emotion/react"
+import { Box, CssBaseline } from "@mui/material"
+import Navbar from "./components/Navbar"
+import { useMemo, useState } from "react"
+import getTheme from "./theme"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [mode, setMode] = useState("light")
+  const theme = useMemo(() => getTheme(mode), [mode])
+
+  const toggleMode = () => {
+    setMode((prev) => (prev === "light" ? "dark" : "light"))
+  }
+
+  const handleMenuClick = () => {
+    alert("Menu clicked (later we can make this open a drawer)")
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={theme}>
+       <CssBaseline />
+
+      <Box sx={{ minHeight: "100vh", bgcolor: "background.default" }}>
+        <Navbar 
+          onMenuClick={handleMenuClick}
+          mode={mode}
+          onToggleMode={toggleMode}
+        />
+      </Box>
+    </ThemeProvider>
   )
 }
 
